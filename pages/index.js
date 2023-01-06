a7614ff6ae2923dab3d330b6947cae0c55481264
Krintox <shashanksuggala@gmail.com>
2023-01-06 12:55:07 +0530

    changed comma

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div><section class="landing-section" id="landing-section">
     <div class="intro-div">
         <p class="welcome-para">Welcome to my website</p>
-        <p class="webdev-para">I'm a <span>Web</span> Devel<span>op</span>er</p>
+        <p class="webdev-para">I am a <span>Web</span> Devel<span>op</span>er</p>
 
         <div class="hire-btn-div">
             <button class="hire-btn">Hire Me</button>
